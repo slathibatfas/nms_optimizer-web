a776c9ed39fc5c9139cabf7ccaeb86a8ce26a97f
John Belew <john.belew@gmail.com>
2025-03-25 23:26:53 -0700

    A hack for Safari font rendering bug?

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ const App: React.FC = () => {
 
           {/* Header */}
           <Box asChild className="pb-4 optimizer__header text-custom-cyan-light">
-            <Heading as="h1" size="7" className="!font-semibold optimizer__title" style={{ color: "var(--gray-12)" }}>
+            <Heading as="h1" size="7" className="font-normal md:font-bold optimizer__title" style={{ color: "var(--gray-12)" }}>
               No Man's Sky Starship Optimizer <span className="font-extralight" style={{ color: "var(--gray-11)" }}>v0.91α</span>
             </Heading>
           </Box>
