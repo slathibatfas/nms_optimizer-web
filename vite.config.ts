2118f0d43b8b71188f2e60631ed7f96c488cd121
John Belew <john.belew@gmail.com>
2025-05-06 11:40:40 -0700

    One last font fix for performance.

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
     react(),
     tailwindcss(),
     visualizer({ open: true, gzipSize: true, brotliSize: true }) as Plugin, // Added visualizer
+    
   ],
   server: {
     host: '0.0.0.0',  // Listen on all network interfaces
