fc15f045e08cb22685c918c7b3ac665f99ac11b3
John Belew <john.belew@gmail.com>
2025-05-06 16:08:34 -0700

    One more header change.

--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -13,10 +13,11 @@ const AppHeader: React.FC = () => {
       <div className="flex items-center w-full"> {/* Changed items-start to items-center */}
         <img src={NMSIcon} className="mr-4 h-14 sm:mr-4 header__icon sm:h-22" alt="No Man's Sky Icon" />
         <div>
-          <img src={NMSLogo} className="h-5 mb-1 sm:mb-2 sm:h-9 header__logo" alt="No Man's Sky Logo" />
-          <h1 className="text-base sm:text-2xl header__title">
+          <img src={NMSLogo} className="h-5 mb-1 sm:mb-2 sm:h-8 header__logo" alt="No Man's Sky Logo" />
+          {/* <h1 className="text-2xl sm:text-4xl">No Man's Sky</h1> */}
+          <h2 className="text-base sm:text-2xl header__title">
             <strong>Neural</strong> Technology Optimizer <span className="font-thin">{APP_VERSION}</span>
-          </h1>
+          </h2>
         </div>
         <div className="flex items-end ml-auto"> {/* Added ml-auto */}
           <img src={NMSShip} className="hidden opacity-25 md:h-14 lg:h-18 md:inline fade-horizontal" />
