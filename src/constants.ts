24245bf16afb658db809b2de5819b10dee57fa5d
John Belew <john.belew@gmail.com>
2025-05-21 10:53:27 -0700

    One last App title change for better SEO.

--- src/constants.ts
+++ src/constants.ts
@@ -2,4 +2,4 @@
 export const API_URL = import.meta.env.VITE_API_URL;
 export const TRACKING_ID = "G-P5VBZQ69Q9"; // Google Analytics Tracking ID
 export const APP_VERSION = "v2.75.1";
-export const APP_NAME = "No Man's Sky AI Build Optimizer";
+export const APP_NAME = "No Man's Sky AI Technology Layout Optimizer";
