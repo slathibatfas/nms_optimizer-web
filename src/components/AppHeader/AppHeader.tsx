d35e92be62811fb0e60769ea2dbdd21f5c6b2161
John Belew <john.belew@gmail.com>
2025-05-22 17:37:24 -0700

    Some minor header spacing changes.

--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -14,7 +14,7 @@ const AppHeaderInternal: React.FC<AppHeaderInternalProps> = ({ onShowChangelog }
   return (
     <header className="flex flex-col pt-3 pb-1 pl-6 sm:pr-8 header sm:pb-4 sm:pt-6">
       <div className="flex items-center w-full">
-        <img src={NMSIcon} className="mr-4 h-[56] w-[32] sm:mr-4 header__icon sm:h-[88] sm:w-[50]" alt="No Man's Sky Icon" />
+        <img src={NMSIcon} className="mr-2 h-[56] w-[32] sm:mr-4 header__icon sm:h-[88] sm:w-[50]" alt="No Man's Sky Icon" />
         <div>
           <img src="/assets/svg/nms_logo.svg" className="h-[20] w-[234] mb-1 sm:mb-2 sm:h-[32] sm:w-[374] header__logo" alt="No Man's Sky Logo" />
           <h2 className="text-base sm:text-2xl header__title">
