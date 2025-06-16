4d4149d4bd2a5c67ff970ca8c40fccfd11754938
John Belew <john.belew@gmail.com>
2025-06-16 09:15:27 -0700

    Automated and updated manifest screenshots.

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -3,8 +3,7 @@ import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
 
 import de from "./locales/de/translation.json";
-import dev from "./locales/dev/translation.json"; // Optional fallback/dev lang
-// 🗂️ Import bundled JSON files
+import dev from "./locales/dev/translation.json";
 import en from "./locales/en/translation.json";
 import es from "./locales/es/translation.json";
 import fr from "./locales/fr/translation.json";
